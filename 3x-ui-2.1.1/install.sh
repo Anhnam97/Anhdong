d664dfbff1ea2eaea163193ef4d9072acfdf0dc6
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2024-05-25 08:53:00 +0700

    Update install.sh
    Ok

--- 3x-ui-2.1.1/install.sh
+++ 3x-ui-2.1.1/install.sh
@@ -136,14 +136,14 @@ install_x-ui() {
             exit 1
         fi
         echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/Anhnam97/Anhdong/raw/main/${last_version}/x-ui-linux-$(arch3xui).tar.gz
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/Anhnam97/Anhdong/raw/main/$/x-ui-linux-$(arch3xui).tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
         fi
     else
         last_version=$1
-        url="https://github.com/Anhnam97/Anhdong/raw/main/${last_version}/x-ui-linux-$(arch3xui).tar.gz"
+        url="https://github.com/Anhnam97/Anhdong/raw/main/$/x-ui-linux-$(arch3xui).tar.gz"
         echo -e "Beginning to install x-ui $1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz ${url}
         if [[ $? -ne 0 ]]; then
