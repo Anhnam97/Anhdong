ed5012f2c2bb5612291c9219402dd95dc7773543
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2024-06-22 18:27:08 +0700

    Update install.sh
    zzx

--- 3x-ui-2.1.1/install.sh
+++ 3x-ui-2.1.1/install.sh
@@ -129,7 +129,7 @@ config_after_install() {
 install_x-ui() {
     cd /usr/local/
     
-  if [ $# == 0 ]; then
+   if [ $# == 0 ]; then
         last_version=$(curl -Ls "https://api.github.com/repos/MHSanaei/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
         if [[ ! -n "$last_version" ]]; then
             echo -e "${red}Failed to fetch x-ui version, it maybe due to Github API restrictions, please try it later${plain}"
