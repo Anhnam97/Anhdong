b77327a6cd70f25efabb741c8a04ae71a256aa28
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2025-05-03 18:58:34 +0700

    Update adblock.js
    Kksid

--- ổ tập trung/adblock.js
+++ ổ tập trung/adblock.js
@@ -1,16 +1,18 @@
-// adblock.js
 if (typeof $request !== 'undefined') {
   const adKeywords = [
-    "ads", "adservice", "doubleclick",
-    "googlesyndication", "applovin",
-    "adcolony", "unityads", "facebookads"
+    "ads", "adservice", "doubleclick", "googlesyndication",
+    "googletagservices", "taboola", "outbrain",
+    "applovin", "adcolony", "unityads", "facebookads",
+    "tracking", "track", "metrics", "beacons"
   ];
 
-  if (adKeywords.some(keyword => $request.url.includes(keyword))) {
+  if (adKeywords.some(keyword => $request.url.toLowerCase().includes(keyword))) {
+    console.log("Blocked Ad URL: " + $request.url);
     $done({ status: 204, body: "" });
   } else {
     $done({});
   }
 } else {
+  console.log("Loaded in wrong context");
   $done({});
 }
