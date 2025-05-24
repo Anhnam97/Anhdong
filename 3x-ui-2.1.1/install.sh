f32fcbbd04d07caf5faa107aa7666ba152694de1
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2025-05-24 21:54:10 +0700

    Update install.sh
    554

--- 3x-ui-2.1.1/install.sh
+++ 3x-ui-2.1.1/install.sh
@@ -26,12 +26,15 @@ echo "The OS release is: $release"
 arch3xui() {
     case "$(uname -m)" in
     x86_64 | x64 | amd64) echo 'amd64' ;;
+    i*86 | x86) echo '386' ;;
     armv8* | armv8 | arm64 | aarch64) echo 'arm64' ;;
     armv7* | armv7 | arm) echo 'armv7' ;;
-    armv6* | armv6 | arm) echo 'armv6' ;;
+    armv6* | armv6) echo 'armv6' ;;
+    armv5* | armv5) echo 'armv5' ;;
     *) echo -e "${green}Unsupported CPU architecture! ${plain}" && rm -f install.sh && exit 1 ;;
     esac
 }
+
 echo "arch: $(arch3xui)"
 
 os_version=""
@@ -78,18 +81,20 @@ fi
 
 install_base() {
     case "${release}" in
-        centos|fedora|almalinux|rocky)
-            yum -y update && yum install -y -q wget curl tar
-            ;;
-        arch|manjaro)
-            pacman -Syu && pacman -Syu --noconfirm wget curl tar
-            ;;
-        *)
-            apt-get update && apt install -y -q wget curl tar
-            ;;
+    centos | almalinux | rocky)
+        yum -y update && yum install -y -q wget curl tar
+        ;;
+    fedora)
+        dnf -y update && dnf install -y -q wget curl tar
+        ;;
+    arch | manjaro)
+        pacman -Syu && pacman -Syu --noconfirm wget curl tar
+        ;;
+    *)
+        apt-get update && apt install -y -q wget curl tar
+        ;;
     esac
 }
-
 
 # This function will be called when user installed x-ui out of security
 config_after_install() {
@@ -118,9 +123,9 @@ config_after_install() {
             echo -e "${green}username:${usernameTemp}${plain}"
             echo -e "${green}password:${passwordTemp}${plain}"
             echo -e "###############################################"
-            echo -e "${red}if you forgot your login info,you can type x-ui and then type 7 to check after installation${plain}"
+            echo -e "${red}if you forgot your login info,you can type x-ui and then type 8 to check after installation${plain}"
         else
-            echo -e "${red} this is your upgrade,will keep old settings,if you forgot your login info,you can type x-ui and then type 7 to check${plain}"
+            echo -e "${red} this is your upgrade,will keep old settings,if you forgot your login info,you can type x-ui and then type 8 to check${plain}"
         fi
     fi
     /usr/local/x-ui/x-ui migrate
@@ -129,16 +134,21 @@ config_after_install() {
 install_x-ui() {
     cd /usr/local/
 
-    if [ $# -eq 0 ]; then
-        # Không kiểm tra phiên bản, tải tệp trực tiếp
-        echo -e ""
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/Anhnam97/Anhdong/raw/main/%C3%B4%CC%89%20t%C3%A2%CC%A3p%20trung/x-ui-linux-amd64%202.1.1.tar.gz
+    if [ $# == 0 ]; then
+        last_version=$(curl -Ls "https://api.github.com/repos/MHSanaei/3x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
+        if [[ ! -n "$last_version" ]]; then
+            echo -e "${red}Failed to fetch x-ui version, it maybe due to Github API restrictions, please try it later${plain}"
+            exit 1
+        fi
+        echo -e "Got x-ui latest version: ${last_version}, beginning the installation..."
+        wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz https://github.com/MHSanaei/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch3xui).tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}Downloading x-ui failed, please be sure that your server can access Github ${plain}"
             exit 1
         fi
     else
-        url="https://github.com/Anhnam97/Anhdong/raw/main/%C3%B4%CC%89%20t%C3%A2%CC%A3p%20trung/x-ui-linux-amd64%202.1.1.tar.gz"
+        last_version=$1
+        url="https://github.com/MHSanaei/3x-ui/releases/download/${last_version}/x-ui-linux-$(arch3xui).tar.gz"
         echo -e "Beginning to install x-ui $1"
         wget -N --no-check-certificate -O /usr/local/x-ui-linux-$(arch3xui).tar.gz ${url}
         if [[ $? -ne 0 ]]; then
@@ -158,22 +168,22 @@ install_x-ui() {
     chmod +x x-ui
 
     # Check the system's architecture and rename the file accordingly
-    if [[ $(arch3xui) == "armv6" || $(arch3xui) == "armv7" ]]; then
-    mv bin/xray-linux-$(arch3xui) bin/xray-linux-arm
-    chmod +x bin/xray-linux-arm
+    if [[ $(arch3xui) == "armv5" || $(arch3xui) == "armv6" || $(arch3xui) == "armv7" ]]; then
+        mv bin/xray-linux-$(arch3xui) bin/xray-linux-arm
+        chmod +x bin/xray-linux-arm
     fi
-    
+
     chmod +x x-ui bin/xray-linux-$(arch3xui)
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://github.com/Anhnam97/Anhdong/raw/main/3x-ui-2.1.1/x-ui.sh
+    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/MHSanaei/3x-ui/main/x-ui.sh
     chmod +x /usr/local/x-ui/x-ui.sh
     chmod +x /usr/bin/x-ui
     config_after_install
-    
+
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    echo -e "{3X-UI V2.1.1} installation finished, it is running now..."
+    echo -e "${green}x-ui ${last_version}${plain} installation finished, it is running now..."
     echo -e ""
     echo -e "x-ui control menu usages: "
     echo -e "----------------------------------------------"
@@ -191,7 +201,6 @@ install_x-ui() {
     echo -e "x-ui uninstall    - Uninstall x-ui"
     echo -e "----------------------------------------------"
 }
-
 
 echo -e "${green}Running...${plain}"
 install_base
