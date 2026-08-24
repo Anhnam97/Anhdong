0780fc750f09d9e6e9f499d1e59e5120197078cd
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2026-08-24 21:50:15 +0700

    Update TweakLoader.m
    22f5ff

--- TweakLoader_Source/TweakLoader.m
+++ TweakLoader_Source/TweakLoader.m
@@ -1,33 +1,52 @@
 #import <UIKit/UIKit.h>
-static BOOL oldIdleTimerState = NO;
-static void TweakLoaderEnableIdleTimer(void) { dispatch_async(dispatch_get_main_queue(), ^{ UIApplication *app = [UIApplication sharedApplication];
-    oldIdleTimerState = app.idleTimerDisabled;
+static NSTimer *keepScreenOnTimer = nil;
+static void KeepScreenOn(void) { dispatch_async(dispatch_get_main_queue(), ^{ UIApplication *app = [UIApplication sharedApplication];
+    if (app.applicationState == UIApplicationStateActive) {
+        app.idleTimerDisabled = YES;
+    }
+});
+}
+static void StartKeepScreenOn(void) { dispatch_async(dispatch_get_main_queue(), ^{ UIApplication *app = [UIApplication sharedApplication];
     app.idleTimerDisabled = YES;
+
+    [keepScreenOnTimer invalidate];
+
+    keepScreenOnTimer =
+    [NSTimer scheduledTimerWithTimeInterval:5.0
+                                     repeats:YES
+                                       block:^(NSTimer *timer) {
+        KeepScreenOn();
+    }];
 });
 }
-static void TweakLoaderRestoreIdleTimer(void) { dispatch_async(dispatch_get_main_queue(), ^{ UIApplication *app = [UIApplication sharedApplication];
-    app.idleTimerDisabled = oldIdleTimerState;
+static void StopKeepScreenOn(void) { dispatch_async(dispatch_get_main_queue(), ^{ [keepScreenOnTimer invalidate]; keepScreenOnTimer = nil;
+    [UIApplication sharedApplication].idleTimerDisabled = NO;
 });
 }
-attribute((constructor)) static void TweakLoaderInit(void) { dispatch_async(dispatch_get_main_queue(), ^{ NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
+attribute((constructor)) static void TweakLoaderInit(void) {
+dispatch_async(dispatch_get_main_queue(), ^{
+
+    NSNotificationCenter *center =
+        [NSNotificationCenter defaultCenter];
+
     [center addObserverForName:UIApplicationDidBecomeActiveNotification
                         object:nil
                          queue:[NSOperationQueue mainQueue]
                     usingBlock:^(NSNotification *notification) {
-        TweakLoaderEnableIdleTimer();
+        StartKeepScreenOn();
     }];
 
     [center addObserverForName:UIApplicationWillResignActiveNotification
                         object:nil
                          queue:[NSOperationQueue mainQueue]
                     usingBlock:^(NSNotification *notification) {
-        TweakLoaderRestoreIdleTimer();
+        StopKeepScreenOn();
     }];
 
     UIApplication *app = [UIApplication sharedApplication];
 
     if (app.applicationState == UIApplicationStateActive) {
-        app.idleTimerDisabled = YES;
+        StartKeepScreenOn();
     }
 });
 }
