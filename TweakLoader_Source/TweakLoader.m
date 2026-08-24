a53a4b3dc23f4f08ddbc8953bd159d3505c2607c
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2026-08-24 22:14:49 +0700

    Update TweakLoader.m
    Ppajsj

--- TweakLoader_Source/TweakLoader.m
+++ TweakLoader_Source/TweakLoader.m
@@ -1,47 +1,75 @@
 #import <UIKit/UIKit.h>
-static NSTimer *keepScreenOnTimer = nil;
-static void KeepScreenOn(void) { dispatch_async(dispatch_get_main_queue(), ^{ UIApplication *app = [UIApplication sharedApplication];
-    if (app.applicationState == UIApplicationStateActive) {
-        app.idleTimerDisabled = YES;
-    }
-});
-}
-static void StartKeepScreenOn(void) { dispatch_async(dispatch_get_main_queue(), ^{ UIApplication *app = [UIApplication sharedApplication];
-    app.idleTimerDisabled = YES;
 
-    [keepScreenOnTimer invalidate];
+static void AnhdongForceScreenOn(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIApplication *app = [UIApplication sharedApplication];
 
-    keepScreenOnTimer =
-    [NSTimer scheduledTimerWithTimeInterval:5.0
-                                     repeats:YES
-                                       block:^(NSTimer *timer) {
-        KeepScreenOn();
-    }];
-});
+        if (app.applicationState == UIApplicationStateActive) {
+            app.idleTimerDisabled = YES;
+        }
+    });
 }
-static void StopKeepScreenOn(void) { dispatch_async(dispatch_get_main_queue(), ^{ [keepScreenOnTimer invalidate]; keepScreenOnTimer = nil;
-    [UIApplication sharedApplication].idleTimerDisabled = NO;
-});
+
+static void AnhdongShowLoaded(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        UIAlertController *alert =
+            [UIAlertController alertControllerWithTitle:@"Anhdong"
+                                                message:@"Anhdong Tweak: ON"
+                                         preferredStyle:UIAlertControllerStyleAlert];
+
+        [alert addAction:
+            [UIAlertAction actionWithTitle:@"OK"
+                                     style:UIAlertActionStyleDefault
+                                   handler:nil]];
+
+        UIViewController *root = nil;
+
+        for (UIScene *scene in
+             [UIApplication sharedApplication].connectedScenes) {
+
+            if (scene.activationState == UISceneActivationStateForegroundActive &&
+                [scene isKindOfClass:[UIWindowScene class]]) {
+
+                UIWindowScene *windowScene = (UIWindowScene *)scene;
+
+                for (UIWindow *window in windowScene.windows) {
+                    if (window.isKeyWindow) {
+                        root = window.rootViewController;
+                        break;
+                    }
+                }
+            }
+
+            if (root) break;
+        }
+
+        if (root) {
+            [root presentViewController:alert
+                                animated:YES
+                              completion:nil];
+        }
+    });
 }
-__attribute__((constructor)) static void TweakLoaderInit(void) { dispatch_async(dispatch_get_main_queue(), ^{ NSNotificationCenter *center = [NSNotificationCenter defaultCenter];
-    [center addObserverForName:UIApplicationDidBecomeActiveNotification
+
+__attribute__((constructor))
+static void AnhdongTweakInit(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        AnhdongForceScreenOn();
+
+        AnhdongShowLoaded();
+
+        [[NSNotificationCenter defaultCenter]
+            addObserverForName:UIApplicationDidBecomeActiveNotification
                         object:nil
                          queue:[NSOperationQueue mainQueue]
-                    usingBlock:^(NSNotification *notification) {
-        StartKeepScreenOn();
-    }];
+                    usingBlock:^(NSNotification *note) {
+            AnhdongForceScreenOn();
+        }];
 
-    [center addObserverForName:UIApplicationWillResignActiveNotification
-                        object:nil
-                         queue:[NSOperationQueue mainQueue]
-                    usingBlock:^(NSNotification *notification) {
-        StopKeepScreenOn();
-    }];
-
-    UIApplication *app = [UIApplication sharedApplication];
-
-    if (app.applicationState == UIApplicationStateActive) {
-        StartKeepScreenOn();
-    }
-});
+        [NSTimer scheduledTimerWithTimeInterval:2.0
+                                         repeats:YES
+                                           block:^(NSTimer *timer) {
+            AnhdongForceScreenOn();
+        }];
+    });
 }
