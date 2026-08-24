50a53efe71615b798553d2a449a24996d7e98f41
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2026-08-24 22:24:41 +0700

    Update TweakLoader.m
    Pâpodhhd

--- TweakLoader_Source/TweakLoader.m
+++ TweakLoader_Source/TweakLoader.m
@@ -2,25 +2,28 @@
 
 static void KeepScreenOn(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        [UIApplication sharedApplication].idleTimerDisabled = YES;
+        UIApplication *app = [UIApplication sharedApplication];
+        app.idleTimerDisabled = YES;
     });
 }
 
-%ctor {
-    KeepScreenOn();
-
-    [[NSNotificationCenter defaultCenter]
-        addObserverForName:UIApplicationDidBecomeActiveNotification
-                    object:nil
-                     queue:[NSOperationQueue mainQueue]
-                usingBlock:^(NSNotification *notification) {
-                    KeepScreenOn();
-                }];
-
-    // Kiểm tra lại định kỳ để giữ màn hình luôn sáng
-    [NSTimer scheduledTimerWithTimeInterval:5.0
-                                     repeats:YES
-                                       block:^(NSTimer *timer) {
+__attribute__((constructor))
+static void TweakLoaderInit(void) {
+    dispatch_async(dispatch_get_main_queue(), ^{
         KeepScreenOn();
-    }];
+
+        [[NSNotificationCenter defaultCenter]
+            addObserverForName:UIApplicationDidBecomeActiveNotification
+                        object:nil
+                         queue:[NSOperationQueue mainQueue]
+                    usingBlock:^(NSNotification *notification) {
+            KeepScreenOn();
+        }];
+
+        [NSTimer scheduledTimerWithTimeInterval:5.0
+                                         repeats:YES
+                                           block:^(NSTimer *timer) {
+            KeepScreenOn();
+        }];
+    });
 }
