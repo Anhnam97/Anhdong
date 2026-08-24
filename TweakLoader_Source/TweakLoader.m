8b7709f8e7ab0ff3562ac8b714f2e3fa2d966c2e
Anhnam97 <121592635+Anhnam97@users.noreply.github.com>
2026-08-24 22:21:51 +0700

    Update TweakLoader.m
    Ppjhhv

--- TweakLoader_Source/TweakLoader.m
+++ TweakLoader_Source/TweakLoader.m
@@ -1,75 +1,26 @@
 #import <UIKit/UIKit.h>
 
-static void AnhdongForceScreenOn(void) {
+static void KeepScreenOn(void) {
     dispatch_async(dispatch_get_main_queue(), ^{
-        UIApplication *app = [UIApplication sharedApplication];
-
-        if (app.applicationState == UIApplicationStateActive) {
-            app.idleTimerDisabled = YES;
-        }
+        [UIApplication sharedApplication].idleTimerDisabled = YES;
     });
 }
 
-static void AnhdongShowLoaded(void) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        UIAlertController *alert =
-            [UIAlertController alertControllerWithTitle:@"Anhdong"
-                                                message:@"Anhdong Tweak: ON"
-                                         preferredStyle:UIAlertControllerStyleAlert];
+%ctor {
+    KeepScreenOn();
 
-        [alert addAction:
-            [UIAlertAction actionWithTitle:@"OK"
-                                     style:UIAlertActionStyleDefault
-                                   handler:nil]];
+    [[NSNotificationCenter defaultCenter]
+        addObserverForName:UIApplicationDidBecomeActiveNotification
+                    object:nil
+                     queue:[NSOperationQueue mainQueue]
+                usingBlock:^(NSNotification *notification) {
+                    KeepScreenOn();
+                }];
 
-        UIViewController *root = nil;
-
-        for (UIScene *scene in
-             [UIApplication sharedApplication].connectedScenes) {
-
-            if (scene.activationState == UISceneActivationStateForegroundActive &&
-                [scene isKindOfClass:[UIWindowScene class]]) {
-
-                UIWindowScene *windowScene = (UIWindowScene *)scene;
-
-                for (UIWindow *window in windowScene.windows) {
-                    if (window.isKeyWindow) {
-                        root = window.rootViewController;
-                        break;
-                    }
-                }
-            }
-
-            if (root) break;
-        }
-
-        if (root) {
-            [root presentViewController:alert
-                                animated:YES
-                              completion:nil];
-        }
-    });
-}
-
-__attribute__((constructor))
-static void AnhdongTweakInit(void) {
-    dispatch_async(dispatch_get_main_queue(), ^{
-        AnhdongForceScreenOn();
-
-        AnhdongShowLoaded();
-
-        [[NSNotificationCenter defaultCenter]
-            addObserverForName:UIApplicationDidBecomeActiveNotification
-                        object:nil
-                         queue:[NSOperationQueue mainQueue]
-                    usingBlock:^(NSNotification *note) {
-            AnhdongForceScreenOn();
-        }];
-
-        [NSTimer scheduledTimerWithTimeInterval:2.0
-                                         repeats:YES
-                                           block:^(NSTimer *timer) {
-            AnhdongForceScreenOn();
-        }];
-    });
+    // Kiểm tra lại định kỳ để giữ màn hình luôn sáng
+    [NSTimer scheduledTimerWithTimeInterval:5.0
+                                     repeats:YES
+                                       block:^(NSTimer *timer) {
+        KeepScreenOn();
+    }];
 }
